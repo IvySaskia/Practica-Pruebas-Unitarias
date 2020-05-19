9c86c71515b29f00513151e04e9fc0b8c0c1ba6e
IvySaskia <saskiaivy@outlook.com>
2020-05-19 17:08:46 -0400

    toStringProductTest

--- FormReceipt/test/Models/ProductTest.java
+++ FormReceipt/test/Models/ProductTest.java
@@ -3,13 +3,13 @@ package Models;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class ProductTest {
 		
 	private Product product = new Product();
-	
+	private Product productWithConstructor = new Product("The product is the best on the market",10);
+
 	@Test
 	void getUnitPriceTest() {
 		product.setUnitPrice(5);
@@ -20,5 +20,10 @@ class ProductTest {
 	void getDescriptionTest() {
 		product.setDescription("The product has the best mark and prime materia");
 		assertEquals("The product has the best mark and prime materia",product.getDescription());
+	}
+	
+	@Test
+	void toStringProductTest() {
+		assertEquals("Product{unitPrice=10.0, description=The product is the best on the market}",productWithConstructor.toString());
 	}
 }
